c1208b87b84a2afdcd9500de2fca568429b92af1
Ronny Rosabal <ronnyrosabal@gmail.com>
2015-12-23 19:26:38 -0600

    updates resume project to include all the needed objects

--- catalog/javascript/resume/js/resumeBuilder.js
+++ catalog/javascript/resume/js/resumeBuilder.js
@@ -14,15 +14,90 @@
 var bio = {
   'name' : 'Ronny Rosabal',
   'role' : 'Web Developer',
-  contact : {
-    'phone' : '305.343.8007',
-    'email' : 'ronnyrosabal@aol.com'
+  contacts : {
+    'mobile' : '305.343.8007',
+    'email' : 'ronnyrosabal@aol.com',
+    'github' : 'dineroking',
+    'twitter' : '@dineroking',
+    'location' : 'Houston'
   },
   'picture' : 'images/fry.jpg',
   'skills' : ['amazing', 'crazy', 'strong'],
-  'message' : 'Hello and welcome to my resume.'
+  'welcomeMessage' : 'Hello and welcome to my resume.'
 };
-var formattedRole = HTMLheaderRole.replace( '%data%', bio.role );
+
+var education = {
+  "schools" : [
+    {
+      "name" : "Miami High",
+      "location" : "Miami",
+      'degree' : "High School",
+      "majors" : ["N/A"],
+      "dates" : "09/1997 - 06/1999"
+    },
+    {
+      "name" : "Coral Park",
+      "location" : "Miami",
+      'degree' : "High School",
+      "majors" : ["N/A"],
+      "dates" : "09/1999 - 06/2000"
+    }
+  ],
+  "onlineCourses" : [
+    {
+      "title" : "JavaScript",
+      "school" : "Udacity",
+      "dates" : "present",
+      "url" : "www.udacity.com"
+    },
+    {
+      "title" : "PHP",
+      "school" : "Udemy",
+      "dates" : "09/2005",
+      "url" : "www.udemy.com"
+    }
+  ]
+};
+
+var work = {
+  jobs : [
+    {
+      "employer" : "ExpressJet Airlines",
+      "title" : "Pilot",
+      "location" : "Houston",
+      "dates" : "12/06 - present",
+      "description" : "Safely flying airplanes."
+    },
+    {
+      "employer" : "ADF Airways",
+      "title" : "Instructor",
+      "location" : "Miami",
+      "dates" : "04/04 - 12/06",
+      "description" : "Instruction of future pilots."
+    }
+  ]
+};
+
+var projects = {
+  projects : [
+    {
+      "title" : "unknown",
+      "dates" : "present",
+      "description" : "I have no idea what I'm doing.",
+      "images" : ["url"]
+    },
+    {
+      "title" : "another project",
+      "dates" : "past",
+      "description" : "Another project I did.",
+      "images" : ["url"]
+    }
+  ]
+};
+
+
+//header
+/*var formattedRole = HTMLheaderRole.replace( '%data%', bio.role );
 $( '#header' ).prepend( formattedRole );
 var formatterName = HTMLheaderName.replace( '%data%', bio.name );
 $( '#header' ).prepend( formatterName );
@@ -38,6 +113,25 @@ var formattedSkills = "";
 for(var i = 0; i < bio.skills.length; i++) {
   formattedSkills += HTMLskills.replace( '%data%', bio.skills[i]);
 }
-$('#workExperience' ).append(HTMLskillsStart);
-$('#skills' ).append(formattedSkills);
+$('#header' ).append(HTMLskillsStart);
+$('#header' ).append(formattedSkills);*/
+
+//work info
+/*$('#main').append(HTMLworkStart);
+$('.work-entry').append($('#workExperience'));
+var formattedEmployer = HTMLworkEmployer.replace('%data%', bio['work']['employer']);
+var formattedPosition = HTMLworkTitle.replace('%data%', bio['work']['position']);
+var formattedWorkYears = HTMLworkDates.replace('%data%', bio['work']['years']);
+var formattedWorkCity = HTMLworkLocation.replace('%data%', bio['work']['location']);
+var formattedWorkDescription = HTMLworkDescription.replace('%data%', bio['work']['description']);
+$('#workExperience').append(formattedEmployer, formattedPosition, formattedWorkYears, formattedWorkCity, formattedWorkDescription);*/
+
+//education info
+/*$('#main').append(HTMLschoolStart);
+$('.education-entry').append($('#education'));
+var formattedSchool = HTMLschoolName.replace('%data%', bio.education.school);
+var formattedSchoolYears = HTMLschoolDates.replace('%data%', bio.education.years);
+var formattedSchoolCity = HTMLschoolLocation.replace('%data%', bio.education.location);
+$('#education').append(formattedSchool, formattedSchoolYears, formattedSchoolCity);*/
+
 
