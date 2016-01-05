b45a18b276b85066f1c9febae4efdbca4eebb31c
Ronny Rosabal <ronnyrosabal@gmail.com>
2016-01-05 12:52:38 -0600

    updates resume project

--- catalog/javascript/resume/js/helper.js
+++ catalog/javascript/resume/js/helper.js
@@ -180,6 +180,7 @@ function initializeMap() {
     // hmmmm, I wonder what this is about...
     google.maps.event.addListener( marker, 'click', function() {
       // your code goes here!
+      infoWindow.open(map, marker);
     } );
 
     // this is where the pin actually gets added to the map.
@@ -242,7 +243,7 @@ function initializeMap() {
  */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
