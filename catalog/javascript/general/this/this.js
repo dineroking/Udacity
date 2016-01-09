f4f205cb8660504ff8efa5a232787c0e448d4582
Ronny Rosabal <ronnyrosabal@gmail.com>
2016-01-08 18:48:39 -0600

    updates "this" directory

--- catalog/javascript/general/this/this.js
+++ catalog/javascript/general/this/this.js
@@ -3,5 +3,17 @@
  * By: Ronny Rosabal
  * Date: 1/8/2016
  * Time: 5:57 PM
- * Content:
+ * Content: understanding the "this" parameter (keyword)
  */
+
+var fn = function(a , b){
+  console.log(this);
+};
+
+var fn1 = function(one , two){
+  console.log( one, two );
+};
+
+var r ={}, g ={}, b = {};
+
+fn(g, b);
