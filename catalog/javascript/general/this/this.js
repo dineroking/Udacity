08f4c136abe22e8bbe097bf9bf4c4ccba67c3526
Ronny Rosabal <ronnyrosabal@gmail.com>
2016-01-09 22:34:56 -0600

    finishes the "this" directory

--- catalog/javascript/general/this/this.js
+++ catalog/javascript/general/this/this.js
@@ -7,13 +7,31 @@
  */
 
 var fn = function(a , b){
-  console.log(this);
+  console.log( this, a, b );
+};
+var fn1 = function(a , b){
+  console.log( this, a, b );
 };
 
-var fn1 = function(one , two){
-  console.log( one, two );
-};
+var r ={}, g ={}, b = {}, y = {};
+r.method = fn; //adds function fn as a method of the object r
+console.log("r.method(g,b):");
+r.method( g, b ); //calls the method passing it objects literal g and b as parameters
+//the above method call logs as values three Object. First Object is r with its values because "this" is used in the
+//fn function inside log. Because "this" refers to the object that is to the left of the dot of the function that
+//calls it. Then empty Object g and b are logged.
+console.log("fn(g, b):");
+fn( g, b ); //this logs Window, Object, Object
+console.log("fn1.call(r, g, b):");
+fn1.call(r, g, b); //this like r.method( g, b ); logs Object{} Object{} Object{}
+console.log("r.method.call(y, g, b):");
+r.method.call( y, g, b );
 
-var r ={}, g ={}, b = {};
+/*var random = Math.floor(Math.random() * 6 + 1);
+setInterval(function(){
+  console.log("I show up every " + random + " seconds.");
+  random = Math.floor(Math.random() * 6 + 1);
+}, random * 1000);*/
 
-fn(g, b);
+console.log("new r.method(g, b):");
+new r.method(g, b);
